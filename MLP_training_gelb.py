5d98ed4cff6e22798fb0685890a085c7871951db
PackageMachine <116561868+PackageMachine@users.noreply.github.com>
2022-11-29 14:28:15 +0100

    update training alex danke

--- MLP_training_gelb.py
+++ MLP_training_gelb.py
@@ -1,8 +1,9 @@
 # Importiere benötigte Bibliotheken
+import matplotlib.pyplot as plt
 import sklearn.datasets as skl_data
 import sklearn.neural_network as skl_nn
 import pickle
-
+import numpy as np
 from skimage import io
 
 # Datenimport (MNIST Datensatz)
@@ -16,17 +17,23 @@ data_test = data[150:]
 labels_test = labels[150:]
 
 test_digit = data_test.iloc[0].to_numpy()
-
+setp={1,5,10,25,50}
 # Save example test image to disk
 # io.imsave('img/8.png', data_test.iloc[1].to_numpy().reshape(28,28))
-
+mse=[]
 # Instantiiere MLP Classifier
-mlp = skl_nn.MLPClassifier(hidden_layer_sizes=(50,), max_iter=50, verbose=1, random_state=1)
+for e in range(0,4):
+    for i in range(0,11):
+        mlp = skl_nn.MLPClassifier(hidden_layer_sizes=(steps[e],), max_iter=200, verbose=1, random_state=1)
+        mse.append(sklearn.metrics.mean_squared_error, steps[e])
 
 # Training
-mlp.fit(data_train.values,labels_train)
+plt.boxplot(step,mse)
+plt.show()
+mlp.fit(data_train.values, labels_train)
+pickle.dump(mlp, open("MLP_classifier_gelb", 'wb'))
 print("Training set score", mlp.score(data_train, labels_train))
 print("Testing set score", mlp.score(data_test, labels_test))
 
 # Speichere Netzwerk auf der Platte
-pickle.dump(mlp, open("MLP_classifier_gelb", 'wb'))
+
