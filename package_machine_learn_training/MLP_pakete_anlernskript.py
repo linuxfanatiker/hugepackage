e2d21f30e599f31148b768e5a9c58ec6cf3010bb
Jan Huebchen <jan@huebchen.com>
2022-11-30 11:38:03 +0100

    Testdaten rev2

--- package_machine_learn_training/MLP_pakete_anlernskript.py
+++ package_machine_learn_training/MLP_pakete_anlernskript.py
@@ -19,7 +19,27 @@ labels_train = []
 data_test = []
 labels_test = []
 
-with open('samples_labels.csv', newline='') as csvfile:
+with open('sample_labels_rev1.csv', newline='') as csvfile:
+	complete_csv = csv.reader(csvfile, delimiter=',')
+	for row in complete_csv:
+		complete_content.append([ [float(row[1])], row[2] ])
+
+	random.shuffle(complete_content)
+	no_samples=len(complete_content)
+	ninety_percent=no_samples*0.9
+	i = 0
+
+	while i < ninety_percent:
+		data_train.append(complete_content[i][0])
+		labels_train.append(complete_content[i][1])
+		i=i+1
+
+	while i < no_samples:
+		data_test.append(complete_content[i][0])
+		labels_test.append(complete_content[i][1])
+		i=i+1
+
+with open('sample_labels_rev2.csv', newline='') as csvfile:
 	complete_csv = csv.reader(csvfile, delimiter=',')
 	for row in complete_csv:
 		complete_content.append([ [float(row[1])], row[2] ])
