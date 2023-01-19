cdbbb37ee3331ee972d24845b2287918189853df
Jan Huebchen <jan@huebchen.com>
2023-01-19 11:49:45 +0100

    lcd driver implemented

--- hugepackage - Complete/package_pi_fotoapparat_skripte/pi_paketerkennung.sh
+++ hugepackage - Complete/package_pi_fotoapparat_skripte/pi_paketerkennung.sh
@@ -12,11 +12,19 @@ DEBUG=$1
 I=0
 KEY=1
 
+function display() {
+	ZEILE1=$1
+	ZEILE2=$2
+	echo $ZEILE1 >>/tmp/log
+	echo $ZEILE2 >>/tmp/log
+}
+
 pigs modes 23 r
 pigs modes 18 w
 pigs w 18 0
 
 while [ true ]; do
+  display "Taste druecken" "fuer Erkennung"
   while [ $KEY -ge 1 ]
   do
      KEY=`pigs r 23`
@@ -24,6 +32,7 @@ while [ true ]; do
   echo "Warte auf Ausloeser"
   echo "Ausloeser gedrueckt"
   KEY=1
+  display "Erkennung laeuft" "Bitte warten"
 # Beleuchtung einschalten
   pigs w 18 1
 # Aufnahme starten
@@ -46,20 +55,26 @@ while [ true ]; do
   echo "Erkennung laufen lassen"
   python ./mlp_inference_durchfuehren.py $GELBEPIXEL $REIHE $UEBER_MAX $UNTER_MAX
   PREDICTION=$?
+  ERKANNT="Erkanntes Paket"
   if [ "$PREDICTION" == "1" ]; then
   	echo "Paketgroesse XS"
-	PAKET="XS"
+	PAKET="Paket XS"
   elif [ "$PREDICTION" == "2" ]; then
 	echo "Paketgroesse S"
-	PAKET="S"
+	PAKET="Paket S"
   elif [ "$PREDICTION" == "3" ]; then
 	echo "Paketgroesse M"
-	PAKET="M"
+	PAKET="Paket M"
   else
 	echo "Es wurde kein gelbes Paket erkannt"
+	ERKANNT="Es wurde kein"
+	PAKET="Paket erkannt"
   fi
-	echo "Erkanntes Paket" > /tmp/log
-	echo "Groesse $PAKET" > /tmp/log
+
+	display $ERKANNT $PAKET
+
+	sleep 5
+
   if [ "$DEBUG" = "DEBUG" ]; then
   kill $!
   python ./gelbanteil_debug.py $FILENAME &
